edc226e62fc529179e7c789182b57ba24639407e
Dsdroid1 <dhruvsharma2031@gmail.com>
2021-02-16 09:11:28 +0530

    Tweaked popup,added submit button style

--- src/try2/popup.jsx
+++ src/try2/popup.jsx
@@ -21,14 +21,14 @@ function Popup(props){
                 <input type='text' name='message' placeholder='Enter your message here'/>
                 <br/>
                 <br/>
-                <input style={{ position:'absolute',right:'20px'}} type='submit' value='Submit'></input>
+                <input style={{ position:'absolute',right:'30px'}} type='submit' value='Submit'></input>
                 </form>
                 <br/>
                 <br/>
                 <span style={{color: '#495057', fontSize: '12px'}}>Need more info? hello@newzera.com</span>
-                <a href='https://twitter.com/?lang=en'><div className='twitter icon'></div></a>
-                <a href='https://www.linkedin.com/feed/'><div className='linkedin icon'></div></a>
-                <a href='https://www.facebook.com/'><div className='facebook icon'></div></a>
+                <a href='https://twitter.com/?lang=en'><div className='twitter icons'></div></a>
+                <a href='https://www.linkedin.com/feed/'><div className='linkedin icons'></div></a>
+                <a href='https://www.facebook.com/'><div className='facebook icons'></div></a>
                 
             </div>
         </div>
