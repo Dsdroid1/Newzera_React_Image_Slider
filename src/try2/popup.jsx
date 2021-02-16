636f7172a29039ace83dda6acf343ba3735b3abf
Dsdroid1 <dhruvsharma2031@gmail.com>
2021-02-16 09:41:07 +0530

    Popup css done

--- src/try2/popup.jsx
+++ src/try2/popup.jsx
@@ -9,23 +9,23 @@ function Popup(props){
                 <div className='contact-heading'>Contact</div>
                 <p>Lorem Ipsum is simply dummy text of the printing</p>
                 <form>
-                <label for='name'  >Name    :</label>
+                    <div className='form-field'>
+                <label for='name'  >Name</label>
                 <input type='text' name='name' placeholder='Enter your name here'/>
-                <br/>
-                <br/>
-                <label for='email'  >Email  :</label>
-                <input type='email' name='email' placeholder='Enter your email here'/>
-                <br/>
-                <br/>
-                <label for='message'>Message:</label>
-                <input type='text' name='message' placeholder='Enter your message here'/>
-                <br/>
-                <br/>
+                    </div>
+                    <div className='form-field'>
+                <label for='email'  >Email</label>
+                <input type='email' name='email' placeholder='Enter your e-mail address here'/>
+                    </div>
+                    <div className='form-field'>
+                <label for='message'>Message</label>
+                <input type='text' name='message' placeholder='Wanna share something with us?'/>
+                    </div>
                 <input style={{ position:'absolute',right:'30px'}} type='submit' value='Submit'></input>
                 </form>
                 <br/>
                 <br/>
-                <span style={{color: '#495057', fontSize: '12px'}}>Need more info? hello@newzera.com</span>
+                <span style={{color: '#495057', fontSize: '12px',position:'relative',top: '15px'}}>Need more info? hello@newzera.com</span>
                 <a href='https://twitter.com/?lang=en'><div className='twitter icons'></div></a>
                 <a href='https://www.linkedin.com/feed/'><div className='linkedin icons'></div></a>
                 <a href='https://www.facebook.com/'><div className='facebook icons'></div></a>
